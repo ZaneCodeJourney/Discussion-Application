920f60c4e3253efc0d67c4bfd2a2f0d240c152dd
Ziyang Wen <wenziyang007@gmail.com>
2024-01-26 15:23:51 +0800

    path helper functions

---
+++ src/path.ts
@@ -0,0 +1,14 @@
+const paths = {
+  home() {
+    return "/";
+  },
+  topicShow(topicSlug: string) {
+    return `/topics/${topicSlug}`;
+  },
+  topicCreate(topicSlug: string) {
+    return `/topics/${topicSlug}/posts/new`;
+  },
+  postShow(topicSlug: string, postId: string) {
+    return `/topics/${topicSlug}/posts/${postId}`;
+  },
+};
