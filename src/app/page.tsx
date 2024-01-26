0b312d8e45f084fd2ac5f95b2446b4629a7afc33
Ziyang Wen <wenziyang007@gmail.com>
2024-01-26 12:38:47 +0800

    set up authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click me!!!</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+
+      <Profile />
     </div>
   );
 }
