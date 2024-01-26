6b8f4f00f605626c5ee7803196d45f462ba179fe
Ziyang Wen <wenziyang007@gmail.com>
2024-01-26 19:30:26 +0800

    create topic

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,3 +1,23 @@
 "use server";
 
-export async function createTopic() {}
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lowercase letters or dashes without",
+    }),
+  descrition: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
+}
---
+++ src/components/topics/topic-create-form.tsx
@@ -0,0 +1,39 @@
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Textarea,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+
+export default function TopicCreateForm() {
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe your topic"
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
