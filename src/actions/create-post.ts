3f5be94083e4aa64a619bef3ddeb4cb4f7d52697
Ziyang Wen <wenziyang007@gmail.com>
2024-01-27 23:45:41 +0800

    create posts

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,3 +1,53 @@
 "use server";
 
-export async function createPost() {}
+import type { Post } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
+
+  // TODO: revalidate the topic show page
+}
--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,23 @@
-export default function TopicShowPage() {
-  return <div>Topic show</div>;
+import PostCreateForm from "@/components/posts/post-create-form";
+
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div>
+        <PostCreateForm />
+      </div>
+    </div>
+  );
 }
