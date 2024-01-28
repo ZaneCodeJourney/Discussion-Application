8dceecc5be764d809858e159b2eb1a64c181bc54
Ziyang Wen <wenziyang007@gmail.com>
2024-01-28 22:28:54 +0800

    add two buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,6 +9,8 @@ import {
 import HeaderAuth from "./header-auth";
 import SearchInput from "./search-input";
 import { Suspense } from "react";
+import { DiGithubBadge } from "react-icons/di";
+import { BsLinkedin } from "react-icons/bs";
 
 export default function Header() {
   return (
@@ -27,6 +29,32 @@ export default function Header() {
       </NavbarContent>
 
       <NavbarContent justify="end">
+        <NavbarItem className="flex items-center space-x-4">
+          {" "}
+          {/* 这将创建一个横向布局并在图标之间添加空间 */}
+          <Link
+            href="https://github.com/ZaneCodeJourney"
+            passHref
+            target="_blank"
+          >
+            {/* Tailwind CSS 用来增加图标大小的类 */}
+            <a className="text-5xl text-grey-500">
+              <DiGithubBadge />
+            </a>{" "}
+            {/* 这里的 text-5xl 可以根据您的需要调整大小 */}
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/ziyang-wen-217082276/"
+            passHref
+            target="_blank"
+          >
+            {/* 同样，增加 LinkedIn 图标的大小 */}
+            <a className="text-5xl text-blue-700">
+              <BsLinkedin />
+            </a>{" "}
+            {/* 调整 text-5xl 以改变大小 */}
+          </Link>
+        </NavbarItem>
         <HeaderAuth />
       </NavbarContent>
     </Navbar>
