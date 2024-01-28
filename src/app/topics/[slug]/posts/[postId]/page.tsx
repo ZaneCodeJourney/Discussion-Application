f2b346fe493323f081d947a7e8ab53a643be8b58
Ziyang Wen <wenziyang007@gmail.com>
2024-01-28 15:14:14 +0800

    show posts on home page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -4,6 +4,7 @@ import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 import { fetchCommentByPostId } from "@/db/queries/comments";
+import { Suspense } from "react";
 
 interface PostShowPageProps {
   params: {
@@ -20,7 +21,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       <CommentList postId={postId} />
     </div>
--- src/components/posts/post-show.tsx
+++ src/components/posts/post-show.tsx
@@ -1,11 +1,13 @@
-import { db } from "@/db";
 import { notFound } from "next/navigation";
+import { db } from "@/db";
 
 interface PostShowProps {
   postId: string;
 }
 
 export default async function PostShow({ postId }: PostShowProps) {
+  await new Promise((resolve) => setTimeout(resolve, 2500));
+
   const post = await db.post.findFirst({
     where: { id: postId },
   });
