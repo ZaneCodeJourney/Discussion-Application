8d80818ce516709de0b04697e5b8af804c6c3fe6
Ziyang Wen <wenziyang007@gmail.com>
2024-01-28 20:17:58 +0800

    implement search function

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,8 @@ import {
   Input,
 } from "@nextui-org/react";
 import HeaderAuth from "./header-auth";
+import SearchInput from "./search-input";
+import { Suspense } from "react";
 
 export default function Header() {
   return (
@@ -18,7 +20,9 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify="center">
         <NavbarItem>
-          <Input />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
 
--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -7,6 +7,19 @@ export type PostWithData = Post & {
   _count: { comments: number };
 };
 
+export function fetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    where: {
+      OR: [{ title: { contains: term } }, { content: { contains: term } }],
+    },
+  });
+}
+
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
     where: { topic: { slug } },
